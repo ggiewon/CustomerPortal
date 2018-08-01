111b9cd09b5c1cabad08695ea667095939570fa0
ggiewon <ggiewon@gmail.com>
2018-08-01 09:33:49 +0200

    Initial UI interface

---
+++ CustomerPortal.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomerPortal.UI.MvcApplication" Language="C#" %>
---
+++ CustomerPortal.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerPortal.UI.Startup))]
+namespace CustomerPortal.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
